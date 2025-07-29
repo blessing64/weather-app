6f7950eb25bb44b7494edcc2e4c37c81b79818e8
Blessing <blessingshaibu673@gmail.com>
2025-07-29 12:15:45 +0100

    updated

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ country = document.querySelector(".country")
 loc = document.querySelector(".location")
 temp = document.querySelector(".temp")
 text = document.querySelector(".text")
+img = document.querySelector("img")
 
 
 btn.addEventListener("click", () => {
@@ -13,15 +14,23 @@ btn.addEventListener("click", () => {
     if (!user_location) {
         return alert("please enter a city")
     }
-    fetch(`http://api.weatherapi.com/v1/current.json?key=747345deb072451b900113052252407&q=${user_location}&aqi=no`)
+    fetch(`https://api.weatherapi.com/v1/current.json?key=747345deb072451b900113052252407&q=${user_location}&aqi=no`)
         .then((response) => response.json())
         .then((data) => {
             //result.classList. remove("result")
             loc.textContent = data.location.name
-            counrty.textcontent = data.location.name
+            country.textContent = data.location.country
             //icon.src = data.current .condition .icon
             text.textContent = data.current.condition.text
             temp.textContent = Math.trunc(Number(data.current.temp_c))
+            img.src="https:"+data.current.condition.icon
+
         })
 
+})
+let htmlbody = document.querySelector("body")
+let changeModeBtn = document.querySelector(".mode")
+
+changeModeBtn.addEventListener("click",() =>{
+    htmlbody.classList.toggle("NightMode")
 })
